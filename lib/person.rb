5e5d289e4ec5c28ced20b1398945f2eb084ef1f0
David <Davidmaxsands@gmail.com>
2022-02-24 11:01:03 -0700

    commit tree 5b0e2b197eb3aec2dea04b3ca00d4c6b9e203b7a

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,75 @@
-# your code goes here
+class Person
+    attr_accessor :bank_account
+    attr_reader :name, :happiness, :hygiene
+    
+    def initialize(name, bank_account = 25, happiness = 8, hygiene = 8)
+        @name=name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    def happiness=(happiness)
+        @happiness = happiness.clamp(0,10)
+    end
+
+    def hygiene=(hygiene)
+        @hygiene = hygiene.clamp(0,10)
+    end
+
+    def clean?
+        self.hygiene > 7 ? true : false
+    end
+
+    def happy?
+        self.happiness > 7 ? true : false
+    end
+
+    def get_paid amount 
+        self.bank_account += amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        self.hygiene += 4
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        self.hygiene -= 3
+        self.happiness += 2
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend friend 
+        self.happiness += 3
+        friend.happiness += 3
+        "Hi #{friend.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation friend, topic
+        case topic
+        when "weather"
+            self.happiness += 1
+            friend.happiness += 1
+            "blah blah sun blah rain"
+        when "politics"
+            self.happiness -= 2
+            friend.happiness -= 2
+            "blah blah partisan blah lobbyist"
+        else
+            "blah blah blah blah blah"
+        end
+    end
+end
+
+david = Person.new("David")
+josh = Person.new("Josh")
+
+# puts david.call_friend(josh)
+# puts david.happiness
+# puts josh.happiness
+
+puts david.start_conversation(josh, 'weather')
+puts david.happiness
+puts josh.happiness
